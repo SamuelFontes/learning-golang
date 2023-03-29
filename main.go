7c3581b48b4b7148a82dd8486b10a1a8f47f4696
SamuelFontes <samugueldias@gmail.com>
2023-03-29 16:14:45 -0500

    Doing things, going places

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var (g int = 33)
 
 func main(){
-	fmt.Printf("Hello World")
+	i := 999999999999999999
+	var s string = "JUSE";
+	fmt.Println(i)
+	fmt.Println(g)
+	fmt.Println(s)
+	fmt.Println(string(i)) // that's bad
+	fmt.Println(strconv.Itoa(i)) // this is good
+	{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}} // nice
 }
